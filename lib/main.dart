c13187ebf06700978c97c66b3e842fb65e1da21e
James-Njeru <jamesnjeru397@gmail.com>
2022-06-25 11:04:33 +0300

    popupmenu button

--- lib/main.dart
+++ lib/main.dart
@@ -2,46 +2,55 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const FlutterApp());
 
-enum SearchType {web, image, news, shopping}
+enum Menu { itemOne, itemTwo, itemThree, itemFour }
 
-class FlutterApp extends StatefulWidget{
+class FlutterApp extends StatefulWidget {
   const FlutterApp({Key? key}) : super(key: key);
 
   @override
   _FlutterAppState createState() => _FlutterAppState();
 }
 
-class _FlutterAppState extends State<FlutterApp>{
-  String dropdownvalue = 'item 1';
-
-  var items = [
-    'item 1', 'item 2', 'item 3', 'item 4', 'item 5',
-  ];
+class _FlutterAppState extends State<FlutterApp> {
+  String _selectedMenu = '';
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Hello!'),
+          actions: <Widget>[
+            PopupMenuButton<Menu>(
+              onSelected: (Menu item) {
+                setState(() {
+                  _selectedMenu = item.name;
+                });
+              },
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<Menu>>[
+                const PopupMenuItem(
+                  child: Text('Item 1'),
+                  value: Menu.itemOne,
+                ),
+                const PopupMenuItem(
+                  child: Text('Text 2'),
+                  value: Menu.itemTwo,
+                ),
+                const PopupMenuItem(
+                  child: Text('Text 3'),
+                  value: Menu.itemThree,
+                ),
+                const PopupMenuItem(
+                  child: Text('Text 4'),
+                  value: Menu.itemFour,
+                ),
+              ],
+            ),
+          ],
         ),
         body: Center(
-          child: DropdownButton(
-            value: dropdownvalue,
-            icon: const Icon(Icons.keyboard_arrow_down),
-            items: items.map((String items){
-              return DropdownMenuItem(
-                value: items,
-                child: Text(items),
-              );
-            }).toList(),
-            onChanged: (String? newValue){
-              setState((){
-                dropdownvalue = newValue!;
-              });
-            },
-          ),
+          child: Text('_selectedMenu: $_selectedMenu'),
         ),
       ),
     );
